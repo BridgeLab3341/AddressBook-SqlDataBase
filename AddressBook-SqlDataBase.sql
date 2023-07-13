f8b8a2b33ebe7ace13e01d0c5863b81f3f5a478a
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-13 13:31:06 +0530

    [somu]Add:Adding Contacts to the Contacts Address Book.

--- AddressBook-SqlDataBase.sql
+++ AddressBook-SqlDataBase.sql
@@ -3,3 +3,9 @@ Create DataBase AddressBook_Service
 
 Create Table Contacts_Book(Id int Primary key identity(1,1), FirstName varchar(20), LastName varchar(20), Addres varchar(100), City varchar(20), Statee varchar(20),Zip varchar(6),PhoneNumber varchar(10),Email Varchar(30)) 
 
+Insert into Contacts_Book(FirstName,LastName,Addres,City,Statee,Zip,PhoneNumber,Email) values('Soma','Shekar','Banglore','Banglore','Karnataka','563746','9808787656','Soma@gmail.com')
+Insert into Contacts_Book(FirstName,LastName,Addres,City,Statee,Zip,PhoneNumber,Email) values('Anil','Kumar','Delhi','Delhi','Delhi','563746','9808787656','Anil@gmail.com')
+Insert into Contacts_Book(FirstName,LastName,Addres,City,Statee,Zip,PhoneNumber,Email) values('Radha','Rani','Mumbai','Mumbai','Maharastra','678667','7876865675','Radh@gmail.com')
+Insert into Contacts_Book(FirstName,LastName,Addres,City,Statee,Zip,PhoneNumber,Email) values('Ganesh','Kumar','Manglore','Manglore','Karnataka','554645','8677566775','Ganesh@gmail.com')
+
+Select * from Contacts_Book
