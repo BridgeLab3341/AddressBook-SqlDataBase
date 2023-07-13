bddc71dee1d93425368af24a9d64f6a817719052
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-13 13:07:04 +0530

    [somu]Add:Creating DataBase for Address Book.

--- AddressBook-SqlDataBase.sql
+++ AddressBook-SqlDataBase.sql
@@ -1,4 +1,1 @@
---UC1
 Create DataBase AddressBook_Service
-Create Table Contacts_Book(Id int Primary key identity(1,1),FirsName varchar(20),LastName varchar(20),
-Address varchar(100),City varchar(20),State varchar(20),Zip varchar(6),PhoneNumber varchar(10),Email varchar(30))
